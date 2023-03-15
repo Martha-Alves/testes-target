147433516c0b217ada5416099b314970bd6373c4
Martha Alves <106412221+Martha-Alves@users.noreply.github.com>
2023-03-15 20:30:43 -0300

    atualizando exercicio 02

--- 02-fibonacci/fibonacci.js
+++ 02-fibonacci/fibonacci.js
@@ -22,11 +22,18 @@ function sumFibs(num) {
     }
   }
 
+  if (fibonacci.includes(result) === true) {
+     return "O resultado da soma é " + result + " e esse número pertence a sequência que foi gerada a partir do número que você inseriu!"
+  } else {
+    return "O resultado da soma é " + result + " e esse número não pertence a sequência que foi gerada a partir do número que você inseriu!"
+  }
 
-
-  return result
 
 }
 
-console.log(sumFibs(4));
+console.log(sumFibs(3));
 
+// 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
+
+// IMPORTANTE:
+// Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
