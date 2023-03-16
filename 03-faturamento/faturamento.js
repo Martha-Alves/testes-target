2d22ed8e459c838f52838e54150fda4daa3bf118
Martha Alves <106412221+Martha-Alves@users.noreply.github.com>
2023-03-16 00:29:18 -0300

    subindo exercicios

--- 03-faturamento/faturamento.js
+++ 03-faturamento/faturamento.js
@@ -1,5 +1,24 @@
 
+function calculaFaturamento(arr, totalDeDias, diasSemFaturamento ){
+    
+    let totalParaMedia = arr.length;
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
+    let soma  = arr.reduce((preVal, atual) => preVal + atual, 0)
+    let media = soma / totalParaMedia
+    let diasTotais = 0;
 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > media) {
+            diasTotais++;
+        }
+    }
+    
+
+    return "O menor valor de faturamente ocorrido em um dia do mês foi R$" + min + ",00 e o maior valor foi R$" + max + ",00. Em " + diasTotais + " dias o valor de faturamento diário foi superior à média mensal";
+}
+
+console.log(calculaFaturamento([10, 20, 9, 80, 50, 70], 30, 4))
 
 // 3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
 // • O menor valor de faturamento ocorrido em um dia do mês;
---
+++ 05-invertendo-str/invertendo.js
@@ -0,0 +1,11 @@
+function invertendoCoisas(str) {
+    
+    let newStr = "";
+
+    for (let i = str.length-1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr
+}
+
+console.log(invertendoCoisas("Teste"))
